068a1e5ff1ae0b525772e71dbc141773167b8a60
LucasOliverGomes <alancsystem@gmail.com>
2019-09-03 13:42:53 -0300

    cadastro

---
+++ frontend/src/components/user/Produtos.jsx
@@ -0,0 +1,183 @@
+import React, { Component } from 'react';
+import Main from '../template/Main';
+import axios from 'axios';
+
+const heardProps = {
+  icon: 'product-hunt',
+  title: 'Produtos',
+  subtitle: 'Cadastro: Incluir, Lista, Alterar e Excluir!'
+};
+
+//Localizando nosso banco
+const baseUrl = 'http://localhost:3001/produto';
+//Estado inicial - Quando sobe a aplicação
+const inicialState = {
+  produto: { produto: '', quantidade: '', valor: '' },
+  list: []
+};
+
+export default class UserCrud extends Component {
+  state = { ...inicialState };
+
+  //Será chamado antes do componente na tela
+  //farei uma chamada no backEnd da lista
+  componentWillMount() {
+    axios(baseUrl).then(resp => {
+      this.setState({ list: resp.data });
+    });
+  }
+
+  clear() {
+    this.setState({ user: inicialState.user });
+  }
+  //Para incluir e alterar
+  save() {
+    const produto = this.state.produto;
+    const method = produto.id ? 'put' : 'post';
+    /*Se id for verdadeiro (existe um id, faça um put),
+        senao um post */
+    const url = produto.id ? `${baseUrl}/${produto.id}` : baseUrl;
+    //Se existe um id atualiza a informação senão baseUrl cria mais um id
+    axios[method](url, produto).then(resp => {
+      //getUpdateLIst será criada
+      const list = this.getUpdateList(resp.data);
+      this.setState({ produto: inicialState.produto, list });
+    });
+  }
+  //Atualizando a lista
+  getUpdateList(produto) {
+    //Cria uma nova lista a partir do filter
+    //u => cria uma lista a separando o id que passou
+    //Unshift coloca esse id na primeira posição do array
+    //return list atualiza a linha 35 que atualiza o estado.
+    const list = this.state.list.filter(u => u.id !== produto.id);
+    list.unshift(produto);
+    return list;
+  }
+
+  updateField(event) {
+    //user será o clone (ou recebe o valor) do estado user
+    //clonamos para não alterar o objeto direatamente
+    const produto = { ...this.state.produto };
+    //seta o que está em input e virá value
+    produto[event.target.name] = event.target.value;
+    //set insere
+    this.setState({ produto });
+  }
+  //Jsx para renderizar o formulário.
+  renderForm() {
+    return (
+      <div className="form">
+        <div className="col-12 col-md-6">
+          <div className="form-group">
+            <label>Produto</label>
+            <input
+              type="text"
+              className="form-control"
+              name="produto"
+              value={this.state.produto.name}
+              onChange={e => this.updateField(e)}
+              placeholder="Nome produto"
+            />
+          </div>
+        </div>
+        <div className="col-12 col-md-6">
+          <div className="form-group">
+            <label>Quantidade</label>
+            <input
+              type="text"
+              className="form-control"
+              name="quantidade"
+              value={this.state.produto.email}
+              onChange={e => this.updateField(e)}
+              placeholder="Quantidade"
+            />
+          </div>
+        </div>
+        <div className="adress">
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>Valor</label>
+              <input
+                type="integer"
+                className="form-control"
+                name="valor"
+                value={this.state.produto.endereco}
+                onChange={e => this.updateField(e)}
+                placeholder="Valor unitário"
+              />
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <div className="col-12 d-flex justify-content-end">
+            <button className="btn btn-primary" onClick={e => this.save(e)}>
+              salvar
+            </button>
+            <button className="btn btn-secundary ml-2" onClick={e => this.clear(e)}>
+              cancelar
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  //Atualizar o estado do objeto
+  load(produto) {
+    this.setState({ produto });
+  }
+
+  remove(produto) {
+    //Deleta na base então repasa a lista atualizando
+    axios.delete(`${baseUrl}/${produto.id}`).then(resp => {
+      const list = this.state.list.filter(u => u !== produto);
+      this.setState({ list });
+    });
+  }
+
+  renderTable() {
+    return (
+      <table className="table mt-4">
+        <thead>
+          <th>ID</th>
+          <th>Nome do Produto</th>
+          <th>Quantidade</th>
+          <th>Valor(R$)</th>
+          <th>Ações</th>
+        </thead>
+        <tbody>{this.renderRows()}</tbody>
+      </table>
+    );
+  }
+  renderRows() {
+    return this.state.list.map(produto => {
+      return (
+        <tr key={produto.id}>
+          <td>{produto.id}</td>
+          <td>{produto.produto}</td>
+          <td>{produto.quantidade}</td>
+          <td>{produto.valor}</td>
+          <td>
+            <button className="btn btn=warning">
+              <i className="fa fa-pencil" onClick={() => this.load(produto)} />
+            </button>
+            <button className="btn btn-danger ml-2">
+              <i className="fa fa-trash" onClick={() => this.remove(produto)} />
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <Main {...heardProps}>
+        {this.renderForm()}
+        {this.renderTable()}
+      </Main>
+    );
+  }
+}
